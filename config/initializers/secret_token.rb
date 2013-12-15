df795b4d791a261b0ac76f588ae0e8c95147c6e3
jogiranjith <ranjith@cognitiveclouds.com>
2013-12-15 23:04:31 +0530

    rails app using postgres heroku

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mystore::Application.config.secret_key_base = '64b134f4be3b665fb801fb37d0daf9791b901a2eaec22bb954841c66cf5e69d2cd80b857e2fa83640c3fe7f505ab5909154d1afa5795faa0399033ae73ac66ba'
